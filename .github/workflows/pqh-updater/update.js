0814810daf41e3442c4e2fe29cfcff138b49137c
sehnauoi <105583853+sehnauoi@users.noreply.github.com>
2022-06-12 13:50:21 +0900

    Update update.js

--- .github/workflows/pqh-updater/update.js
+++ .github/workflows/pqh-updater/update.js
@@ -958,16 +958,16 @@ function get_new_images(data) {
                     const index = manifest.indexOf(file_name),
                         line_end = manifest.indexOf('\n', index),
                         file_data = manifest.substring(index, line_end).split(','),
-                        type = file_name.includes('equipment') || file_name.includes('item') ? 'items' : 'unit_icon';
-//                         decrypted_name = file_name.split('_')[1];                    
-//                     files[file_name] = {
-//                         hash: file_data[1],
-//                         encrypted: path.join(DIRECTORY.SETUP, 'encrypted', `${file_name}.unity3d`),
-//                         // CONVERT unit_icon IMAGE NAME BACK TO 0star RARITY SO IT CAN BE ACCESSED MORE EASILY
-//                         // REASON BEING IS THAT unit_id IS SAVED AS 0star RARITY ID
-//                         decrypted: path.join(DIRECTORY.IMAGE_OUTPUT, type, `${type !== 'unit_icon'
-//                             ? decrypted_name : `${decrypted_name.substring(0, 4)}0${decrypted_name.substring(5)}`}.png`),
-//                     };
+                        type = file_name.includes('equipment') || file_name.includes('item') ? 'items' : 'unit_icon',
+                        decrypted_name = file_name.split('_')[1];                    
+                    files[file_name] = {
+                        hash: file_data[1],
+                        encrypted: path.join(DIRECTORY.SETUP, 'encrypted', `${file_name}.unity3d`),
+                        // CONVERT unit_icon IMAGE NAME BACK TO 0star RARITY SO IT CAN BE ACCESSED MORE EASILY
+                        // REASON BEING IS THAT unit_id IS SAVED AS 0star RARITY ID
+                        decrypted: path.join(DIRECTORY.IMAGE_OUTPUT, type, `${type !== 'unit_icon'
+                            ? decrypted_name : `${decrypted_name}`}.png`),
+                    };
                 });
 
                 // DOWNLOAD ENCRYPTED .unity3d FILES FROM CDN
