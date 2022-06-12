91dc608226be5714718c6014d5cda7eb97bf3b2b
sehnauoi <105583853+sehnauoi@users.noreply.github.com>
2022-06-12 12:43:43 +0900

    Update update.js

--- .github/workflows/pqh-updater/update.js
+++ .github/workflows/pqh-updater/update.js
@@ -896,39 +896,7 @@ function get_new_images(data) {
     return new Promise(async (resolve) => {
         let queue = [];
 
-        // CHECK EQUIPMENT
-        console.log("SEARCHING FOR MISSING ITEM IMAGES...");
-        for (const key in data.equipment) {
-            const equipment = data.equipment[key],
-                id = equipment.id,
-                fragment_id = equipment.fragment.id;
-            // CHECK IF IMAGE ALREADY EXISTS
-            if (!fs.existsSync(path.join(DIRECTORY.IMAGE_OUTPUT, 'items', `${id}.png`)) && id !== "999999") {
-                if (id.substring(0, 2) === "31" || id.substring(0, 2) === "32") {
-                    // EQUIPMENT IS A MEMORY PIECE
-                    queue.push(`item_${id}`);
-                }
-                else {
-                    // REGULAR ITEM, BUSINESS AS USUAL
-                    queue.push(`equipment_${id}`);
-                }
-            }
-            if (!fs.existsSync(path.join(DIRECTORY.IMAGE_OUTPUT, 'items', `${fragment_id}.png`)) && fragment_id !== "999999") {
-                queue.push(`equipment_${fragment_id}`);
-            }
-        }
 
-        // CHECK CHARACTERS
-        console.log("SEARCHING FOR MISSING CHARACTER IMAGES...");
-        for (const key in data.character) {
-            // GET THE 3star+ RARITY IMAGE
-            const unit_3_id = `${key.substring(0, 4)}3${key.substring(5)}`;
-
-            // CHECK IF IMAGE ALREADY EXISTS (UNIT ICON IMAGES ARE SAVED AS THEIR unit_0_id)
-            if (!fs.existsSync(path.join(DIRECTORY.IMAGE_OUTPUT, 'unit_icon', `${key}.png`))) {
-                queue.push(`unit_${unit_3_id}`);
-            }
-        }
 
         // EXTRACT IF THERE ARE NEW FILES
         if (queue.length <= 0) {
