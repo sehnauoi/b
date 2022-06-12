bb2894e74b43688e7d9922ff1aa61da5bb26eefe
sehnauoi <105583853+sehnauoi@users.noreply.github.com>
2022-06-12 18:37:44 +0900

    Update update.js

--- .github/workflows/pqh-updater/update.js
+++ .github/workflows/pqh-updater/update.js
@@ -228,7 +228,10 @@
 
          // GET ALL UNIT DATA
          result = await db.all('SELECR * FROM unit_data');
- 
+         result.forEach((row) => {
+            const unit_id = (row.unit_id).toString();
+         });
+
          // GET ALL EQUIPMENT DATA
          result = await db.all('SELECT * FROM equipment_data');
          result.forEach((row) => {
@@ -938,14 +941,14 @@
          // CHECK CHARACTERS
          console.log("SEARCHING FOR MISSING CHARACTER IMAGES...");
          for (const key in data.character) {
-             // GET THE 3star+ RARITY IMAGE
-             const unit_3_id = `${key.substring(0, 4)}3${key.substring(5)}`;
- 
-             // CHECK IF IMAGE ALREADY EXISTS (UNIT ICON IMAGES ARE SAVED AS THEIR unit_0_id)
-             if (!fs.existsSync(path.join(DIRECTORY.IMAGE_OUTPUT, 'unit_icon', `${key}.png`))) {
-                 queue.push(`unit_${unit_3_id}`);
-             }
+            const unit = data.unit[key],
+                id = unit.id;
+
+            if (!fs.existsSync(path.join(DIRECTORY.IMAGE_OUTPUT, 'unit', `${id}.png`)) && id !== "999999") {
+                queue.push(`unit_${id}`);
+            }
          }
+         
  
          // EXTRACT IF THERE ARE NEW FILES
          if (queue.length <= 0) {
@@ -977,10 +980,8 @@
                      files[file_name] = {
                          hash: file_data[1],
                          encrypted: path.join(DIRECTORY.SETUP, 'encrypted', `${file_name}.unity3d`),
-                         // CONVERT unit_icon IMAGE NAME BACK TO 0star RARITY SO IT CAN BE ACCESSED MORE EASILY
-                         // REASON BEING IS THAT unit_id IS SAVED AS 0star RARITY ID
                          decrypted: path.join(DIRECTORY.IMAGE_OUTPUT, type, `${type !== 'unit_icon'
-                             ? decrypted_name : `${decrypted_name.substring(0, 4)}0${decrypted_name.substring(5)}`}.png`),
+                             ? decrypted_name : `${decrypted_name}`}.png`),
                      };
                  });
  
