1b23291bc0541057cc201f6c715eadad2e732cf0
sehnauoi <105583853+sehnauoi@users.noreply.github.com>
2022-06-14 02:17:19 +0900

    a

--- .github/workflows/pqh-updater/update.js
+++ .github/workflows/pqh-updater/update.js
@@ -967,12 +967,12 @@
          // CHECK CHARACTERS ICON 1 & 3 & 6 star
          console.log("SEARCHING FOR MISSING CHARACTER IMAGES...");
          for (const key in data.unit) {
-            const unit_0 = `${key.substring(0, 4)}0${key.substring(5)}`;
+            // const unit_0 = `${key.substring(0, 4)}0${key.substring(5)}`;
                         
              // CHECK IF IMAGE ALREADY EXISTS
              if (!fs.existsSync(path.join(DIRECTORY.IMAGE_OUTPUT, 'unit_icon', `${key}.png`))) {
                 queue.push(`unit_${key}`);
-                queue.pop(`unit_${unit_0}`);
+                // queue.pop(`unit_${unit_0}`);
             }
          }
 
