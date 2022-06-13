1379c48b34ae355f2630bf400b8d9e924b505ae4
sehnauoi <105583853+sehnauoi@users.noreply.github.com>
2022-06-14 02:08:44 +0900

    a

--- .github/workflows/pqh-updater/update.js
+++ .github/workflows/pqh-updater/update.js
@@ -987,25 +987,25 @@
          // CHECK CHARACTERS ICON 3 & 6 star
          console.log("SEARCHING FOR MISSING CHARACTER IMAGES...");
          for (const key in data.unit) {
-                        
-             // CHECK IF IMAGE ALREADY EXISTS
-             if (!fs.existsSync(path.join(DIRECTORY.IMAGE_OUTPUT, 'unit_icon', `${key}.png`))) {
-                queue.push(`unit_${key}`);
-            }
-         }
-
-         // CHECK CHARACTERS ICON 1 star
-         console.log("SEARCHING FOR MISSING CHARACTER IMAGES...");
-         for (const key in data.character) {
-            const unit_1 = `${key.substring(0, 4)}1${key.substring(5)}`;
             const unit_0 = `${key.substring(0, 4)}0${key.substring(5)}`;
                         
              // CHECK IF IMAGE ALREADY EXISTS
              if (!fs.existsSync(path.join(DIRECTORY.IMAGE_OUTPUT, 'unit_icon', `${key}.png`))) {
-                queue.push(`unit_${unit_1}`),
-                queue.pop(`unit_${unit_0}`);            
+                queue.push(`unit_${key}`),
+                queue.pop(`unit_${unit_0}`);   
             }
          }
+
+        //  // CHECK CHARACTERS ICON 1 star
+        //  console.log("SEARCHING FOR MISSING CHARACTER IMAGES...");
+        //  for (const key in data.character) {
+        //     const unit_1 = `${key.substring(0, 4)}1${key.substring(5)}`;
+                        
+        //      // CHECK IF IMAGE ALREADY EXISTS
+        //      if (!fs.existsSync(path.join(DIRECTORY.IMAGE_OUTPUT, 'unit_icon', `${key}.png`))) {
+        //         queue.push(`unit_${unit_1}`);
+        //     }
+        //  }
 
          // CHECK CHARACTERS CARDS
          console.log("SEARCHING FOR MISSING CHARACTER IMAGES...");
