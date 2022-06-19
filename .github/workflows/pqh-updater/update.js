faae76037dd6ac804b4a1c6823ce051a3339ee37
sehnauoi <105583853+sehnauoi@users.noreply.github.com>
2022-06-19 20:21:49 +0900

    Update update.js

--- .github/workflows/pqh-updater/update.js
+++ .github/workflows/pqh-updater/update.js
@@ -70,7 +70,7 @@
      data.unit = unit_data;
 
      const boss_data = await get_boss_data();
-     data.unit = boss_data;
+     data.boss = boss_data;
 
      const skill_data = await get_skill_data();
      data.skill = skill_data;
@@ -221,7 +221,7 @@
             filename: path.join(DIRECTORY.DATABASE, 'master_jp.db'),
             driver: sqlite3.Database
         });
-        result = await db.all('SELECT * FROM enemy_parameter WHERE unit_id > 300000');
+        result = await db.all('SELECT * FROM enemy_parameter WHERE unit_id < 300000');
         result.forEach((row) => {
             data[`${row.unit_id}`] = {
                 id: `${row.unit_id}`,
@@ -1024,6 +1024,16 @@
             }
          }
 
+         // CHECK BOSS ICON
+         console.log("SEARCHING FOR MISSING BOSS ICON...");
+         for (const key in data.boss) {
+                        
+             // CHECK IF IMAGE ALREADY EXISTS
+             if (!fs.existsSync(path.join(DIRECTORY.IMAGE_OUTPUT, 'unit_icon', `${key}.png`)) && key !== `${key.substring(0, 4)}0${key.substring(5)}`) {
+                queue.push(`unit_${key}`);
+            }
+         }
+
          // CHECK CHARACTERS ICON 1 star
          console.log("SEARCHING FOR MISSING 1 STAR CHARACTERS ICON...");
          for (const key in data.character) {
@@ -1057,10 +1067,10 @@
 
          // CHECK BOSS ICON
          console.log("SEARCHING FOR MISSING BOSS ICON...");
-         for (const key in data.unit) {
+         for (const key in data.boss) {
                         
              // CHECK IF IMAGE ALREADY EXISTS
-             if (!fs.existsSync(path.join(DIRECTORY.IMAGE_OUTPUT, 'boss', `${key}.png`)) && key !== `${key.substring(0, 4)}0${key.substring(5)}`) {
+             if (!fs.existsSync(path.join(DIRECTORY.IMAGE_OUTPUT, 'unit_icon', `${key}.png`)) && key !== `${key.substring(0, 4)}0${key.substring(5)}`) {
                 queue.push(`unit_${key}`);
             }
          }
